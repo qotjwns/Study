84a82d43e759e9c9cd3557a5628d975f88282e62
qotjwns <qotjwns112@cau.ac.kr>
2026-01-01 18:37:45 +0900

    TEST: 기본 설정 완료

---
+++ ML/hellotorch.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+class AE(nn.Module):
+        def __init__(self) -> None:
+            super().__init__()
+            self.conv1 = nn.Conv2d(1, 20, 5)
+            self.conv2 = nn.Conv2d(20, 20, 5)
+
+        def forward(self, x):
+            x = F.relu(self.conv1(x))
+            return F.relu(self.conv2(x))
+
+
+
+x = torch.randn(4,4,4)
+
+def train():
+     pass
+
+print(x.flatten().shape)
