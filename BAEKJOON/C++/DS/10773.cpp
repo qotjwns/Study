1e3e9eed50d7a513ecdd233391a02b89480c8ff2
qotjwns <qotjwns112@cau.ac.kr>
2026-01-12 14:02:33 +0900

    SOLVE: 10773.cpp

---
+++ BAEKJOON/C++/DS/10773.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+
+long long sum_stack_destructive(std::stack<int>& st) {
+    long long sum = 0;
+    while (!st.empty()) {
+        sum += st.top();
+        st.pop();
+    }
+    return sum;
+}
+
+int main(){
+    int K, num;    
+    std::cin >> K;
+    std::stack<int> st;
+    
+    for (size_t i = 0; i < K; i++){
+
+        std::cin >> num;
+        if(num == 0){
+            st.pop();
+        }else{
+            st.push(num);
+        }
+    }
+    std::cout << sum_stack_destructive(st);
+}
