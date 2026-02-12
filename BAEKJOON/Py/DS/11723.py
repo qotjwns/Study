18ebe3676aa8a618d524d3759bd03d6ff7495e84
qotjwns <qotjwns112@cau.ac.kr>
2026-02-12 17:38:39 +0900

    CHORE: GPT풀이 추가

--- BAEKJOON/Py/DS/11723.py
+++ BAEKJOON/Py/DS/11723.py
@@ -1,32 +1,66 @@
 import sys
 input = sys.stdin.readline
 
+S = set()
 M = int(input())
-S = 0
-ALL = (1 << 20) - 1
 
-for _ in range(M):
-    cmd = input().split()
-    op = cmd[0]
+def do_opr(opr: str, num: int | None = None):
+    if opr == "add":
+        S.add(num)
+    elif opr == "remove":
+        S.discard(num)  # 없어도 안전
+    elif opr == "check":
+        print(1 if num in S else 0)
+    elif opr == "toggle":
+        if num in S:
+            S.remove(num)
+        else:
+            S.add(num)
+    elif opr == "all":
+        S.clear()
+        S.update(range(1, 21))
+    elif opr == "empty":
+        S.clear()
 
-    if op == "add":
-        x = int(cmd[1])
-        S |= (1 << (x - 1))
+if __name__ == "__main__":
+    for _ in range(M):
+        ops = input().split()
+        if len(ops) == 2:
+            do_opr(ops[0], int(ops[1]))
+        else:
+            do_opr(ops[0])
 
-    elif op == "remove":
-        x = int(cmd[1])
-        S &= ~(1 << (x - 1))
 
-    elif op == "check":
-        x = int(cmd[1])
-        print(1 if (S & (1 << (x - 1))) else 0)
+# 비트 마스킹
+# import sys
+# input = sys.stdin.readline
 
-    elif op == "toggle":
-        x = int(cmd[1])
-        S ^= (1 << (x - 1))
+# M = int(input())
+# S = 0
+# ALL = (1 << 20) - 1
 
-    elif op == "all":
-        S = ALL
+# for _ in range(M):
+#     cmd = input().split()
+#     op = cmd[0]
 
-    elif op == "empty":
-        S = 0
+#     if op == "add":
+#         x = int(cmd[1])
+#         S |= (1 << (x - 1))
+
+#     elif op == "remove":
+#         x = int(cmd[1])
+#         S &= ~(1 << (x - 1))
+
+#     elif op == "check":
+#         x = int(cmd[1])
+#         print(1 if (S & (1 << (x - 1))) else 0)
+
+#     elif op == "toggle":
+#         x = int(cmd[1])
+#         S ^= (1 << (x - 1))
+
+#     elif op == "all":
+#         S = ALL
+
+#     elif op == "empty":
+#         S = 0
